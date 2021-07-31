7ee3982dca8cc9ca13693bc18e99d391f332d79e
Mohammed Abdel Raouf <mohammed@raouf.me>
2021-07-31 16:43:31 +0200

    fix: rename .prettierrc file

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 120,
-  singleQuote: true,
-  trailingComma: 'all',
-  quoteProps: 'consistent',
-  jsxBracketSameLine: false,
-  arrowParens: 'always',
-  parser: 'typescript'
-};
